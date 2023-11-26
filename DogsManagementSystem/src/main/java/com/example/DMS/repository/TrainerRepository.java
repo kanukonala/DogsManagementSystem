266b749f7e8659cf4a9711093d6a062f698d8ffe
Yagna Venkata Teja Kanukonala <123043536+kanukonala@users.noreply.github.com>
2023-11-25 19:32:01 -0600

    Add files via upload

---
+++ DogsManagementSystem/src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+/*
+ * @ Yagna venkata teja kanukonala S560442
+ */
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
